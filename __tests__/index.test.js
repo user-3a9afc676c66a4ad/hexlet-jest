b61c16419a28e431f6a5a80ef199c624604cefb0
Oleg Osipov <olegish@bk.ru>
2024-12-09 12:17:00 +0300

    add fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,3 +4,13 @@ test('reverse', () => {
   expect(reverse('hello')).toEqual('olleh');
   expect(reverse('')).toEqual('');
 });
+
+test('reverse with log text', () => {
+  // TODO: здесь будет чтение файла и запись его содержимого в константу
+  // const text = 'длинная строка...';
+  // const result = '...';
+  const text = readFixtureFile('data.txt').trim();
+  const expected = readFixtureFile('result.txt').trim();
+
+  expect(reverse(text)).toEqual(result);
+});
